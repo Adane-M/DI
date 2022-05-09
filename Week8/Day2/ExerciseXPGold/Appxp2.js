78566de99bc609b33fbfff6f187292e8c886d6b4
Malasa123 <adanemalasa@gmail.com>
2022-05-10 00:29:44 +0300

    xp w8 d2

---
+++ Week8/Day2/ExerciseXPGold/Appxp2.js
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import Car from './Components/Car';
+import Color from './Components/Color';
+import Events from './Components/Events';
+import Phone from './Components/Phone';
+
+
+//////RENDER PAGE WITH CLASS
+class Appxp2 extends React.Component {
+  constructor() {                                                                                                                                                                                                                  
+    super(); 
+    this.state = {
+        color:''
+    }
+  }
+
+  render() {
+      const carinfo = {name: "Ford", model: "Mustang"};
+      return (
+          <div>
+          <Car model={carinfo.model}  size={'small'}/>
+          <Events />
+          <Phone />
+          <Color />
+      </div>
+    )
+}
+}
+
+
+
+
+export default Appxp2;
